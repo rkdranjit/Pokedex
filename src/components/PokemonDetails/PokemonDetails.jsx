b9521e6af94e33df6f613bc2cbd082f8abe63123
rkdranjit <rkdranjit264@gmail.com>
2023-09-14 23:04:02 +0530

    add custom hooks

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import axios from 'axios';
 import './PokemonDetails.css';
+import usePokemonDetails from '../../hooks/usePokemonDetails';
 
 
 function PokemonDetails(){
     
     const {id}=useParams();
-    const POKEMON_DETAIL_URL = 'https://pokeapi.co/api/v2/pokemon/';
-    const [pokemon,setPokemon] = useState(null);
-
-    async function downloadPokemon(){
-        const response = await axios.get(POKEMON_DETAIL_URL + id);
-        const pokemon = response.data;
-        setPokemon({
-            name: pokemon.name,
-            height : pokemon.height,
-            weight : pokemon.weight,
-            types : pokemon.types,
-            image : pokemon.sprites.other.dream_world.front_default
-        });
-    }
-
-    useEffect( ()=>{
-        downloadPokemon();
-    },[]);
+    const [pokemon] =usePokemonDetails(id);
     return(
         <>
         <h3 className='pokedex-redirect'>
